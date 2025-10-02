5ba91a1617944b4e1be729c54f4a96664bc3b466
martin.lankas <martin.lankas@tul.cz>
2025-10-02 17:30:46 +0200

    #3 základ programu pro výpis řady - vstup pro první číslo

---
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string again = "a";
+while (again == "a")
+
+{
+    Console.Clear();
+    Console.WriteLine("////////////////////////////////");
+    Console.WriteLine("/////  Výpis číselné řady  /////");
+    Console.WriteLine("////////////////////////////////");
+    Console.WriteLine("////////  Martin Lankaš  ///////");
+    Console.WriteLine("////////////////////////////////");
+    Console.WriteLine();
+
+    // VSTUP ČÍSELNÉ HODNOTY DO PROGRAMU - ŠPATNÉ ŘESŠENÍ 
+    // Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+    // VSTUP ČÍSELNÉ HODNOTY DO PROGRAMU - lepší řešení  
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.WriteLine("Nezadali jste celé číslo. Zadejte první číslo celé řady znovu: ");
+    
+    }
+
+
+
+
+
+
+
+
+
+    Console.WriteLine();
+    Console.Write("Pro opakování programu stiskněte klávesu a");
+    Console.ReadLine();
+    again = Console.ReadLine();
+    
+        
+    
+
+
+
+
+
+
+
+
+
+}
+
+
+
