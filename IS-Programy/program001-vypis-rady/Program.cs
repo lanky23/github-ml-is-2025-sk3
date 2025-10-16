48af74580a0f02e43604fee71e0b5d240a1fa340
martin.lankas <martin.lankas@tul.cz>
2025-10-16 16:40:12 +0200

    #3 výpis řady vstup a výstup hodnot

--- IS-Programy/program001-vypis-rady/Program.cs
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -26,6 +26,37 @@ while (again == "a")
     
     }
 
+    Console.Write("Zadejte poslední číslo řady (celé číslo): ");
+    int last;
+    while (!int.TryParse(Console.ReadLine(), out last))
+    {
+        Console.WriteLine("Nezadali jste celé číslo. Zadejte poslední číslo celé řady znovu: ");
+    
+    }
+
+Console.Write("Zadejte diferenci (celé číslo): ");
+    int step;
+    while (!int.TryParse(Console.ReadLine(), out step))
+    {
+        Console.WriteLine("Nezadali jste celé číslo. Zadejte diferenci: ");
+    
+    }
+
+    //výpis zadaných hodnot
+    Console.WriteLine();
+    Console.WriteLine("===============================");
+    Console.WriteLine("Zadali jste tyto hodnoty: ");
+    Console.WriteLine("První číslo řady: {0}", first);
+    Console.WriteLine("Zadali jste poslední číslo: {0}", last);
+    Console.WriteLine("Zadali jste diferenci: {0}", step);
+    Console.WriteLine("===============================");
+    Console.WriteLine("První číslo {0}; {1}; {2};", first,last,step);
+    Console.WriteLine("===============================");
+
+
+//
+
+
 
 
 
@@ -36,7 +67,6 @@ while (again == "a")
 
     Console.WriteLine();
     Console.WriteLine("Pro opakování programu stiskněte klávesu a");
-    Console.ReadLine();
     again = Console.ReadLine();
     
         
