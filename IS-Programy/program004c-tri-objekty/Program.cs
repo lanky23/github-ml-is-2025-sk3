f0a21fb05401d2dcfbf8515cb7d28d77663b8e4b
martin.lankas <martin.lankas@tul.cz>
2025-11-05 13:24:29 +0100

    #7 tři obrazce ukol

--- IS-Programy/program004c-tri-objekty/Program.cs
+++ IS-Programy/program004c-tri-objekty/Program.cs
@@ -131,10 +131,9 @@ while (again == "a")
 
 
     }
-     
 
 
         Console.WriteLine();
         Console.WriteLine("Pro opakování programu stiskněte klávesu a");
-        again = Console.ReadLine();
-}
+    again = Console.ReadLine();
+} 
