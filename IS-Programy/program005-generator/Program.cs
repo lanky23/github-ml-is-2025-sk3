34ea516fa8ba9040c247e0adf8381c5115272f94
martin.lankas <martin.lankas@tul.cz>
2025-11-06 17:00:23 +0100

    #8 generátor + zakladní kod

---
+++ IS-Programy/program005-generator/Program.cs
@@ -0,0 +1,69 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("************************************************");
+    Console.WriteLine("************** generator pseudonáhodných čísel************");
+    Console.WriteLine("************************************************");
+    Console.WriteLine("**************** Martin Lankaš *****************");
+    Console.WriteLine("***************** 5.11.2025 *******************");
+    Console.WriteLine("************************************************");
+    Console.WriteLine();
+
+
+    Console.Write("Zadejte počet generovaných čísel (celé číslo): ");
+    int n;
+    while (!int.TryParse(Console.ReadLine(), out n))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte počet čísel znovu (celé číslo): ");
+    }
+
+    Console.Write("Zadejte dolní mez (celé číslo): ");
+    int lowerBound;
+    while (!int.TryParse(Console.ReadLine(), out lowerBound))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte znovu dolní mez (celé číslo): ");
+    }
+
+    Console.Write("Zadejte horní mez (celé číslo): ");
+    int upperBound;
+    while (!int.TryParse(Console.ReadLine(), out upperBound))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte znovu horní mez (celé číslo): ");
+    }
+
+    Console.WriteLine("\n\n==========================================");
+    Console.WriteLine("Uživatelský vstup: ");
+    Console.WriteLine("Počet čísel: {0}; dolní mez: {1}; horní mez: {2}", n, lowerBound, upperBound);
+    Console.WriteLine("==============================================");
+
+    //deklarace pole 
+
+    int[] myRandNumbers = new int[n];
+
+    //příprava pro generování náhodných čísel 
+
+    Random myRandNumb = new Random();
+
+            Console.WriteLine("Náhodná čísla: ");
+
+            for(int i = 0; i < n; i++) {
+                myRandNumbers[i] = myRandNumb.Next(lowerBound, upperBound + 1);
+                Console.Write("{0}; ", myRandNumbers[i]);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+}
--- IS-Programy/program005-generator/obj/Debug/net9.0/program005-generator.AssemblyInfo.cs
+++ IS-Programy/program005-generator/obj/Debug/net9.0/program005-generator.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("program001-vypis-rady")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("program005-generator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0a21fb05401d2dcfbf8515cb7d28d77663b8e4b")]
-[assembly: System.Reflection.AssemblyProductAttribute("program001-vypis-rady")]
-[assembly: System.Reflection.AssemblyTitleAttribute("program001-vypis-rady")]
+[assembly: System.Reflection.AssemblyProductAttribute("program005-generator")]
+[assembly: System.Reflection.AssemblyTitleAttribute("program005-generator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Vygenerované třídou WriteCodeFragment nástroje MSBuild
